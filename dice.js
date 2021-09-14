86cfbba61317b9ff00c2dc14ad370a941fc854db
Captain_Murtuza <69781075+Mohammad0Murtuza@users.noreply.github.com>
2021-09-14 16:37:22 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,31 @@
+const btn = document.querySelector(".btn");
+btn.addEventListener('click', onclick);
+function onclick(e){
+	e.preventDefault();
+
+	const player1 = document.querySelector(".player1").value;
+	const player2 = document.querySelector(".player2").value;
+	// dice 1
+
+	const randomNumber1 = Math.floor(Math.random() * 6 + 1);
+	const sourceImage1 = "images/dice" + randomNumber1 + ".JPG";
+	const image1 = document.querySelector(".img1").setAttribute("src",sourceImage1 );
+
+	// dice 2
+
+	const randomNumber2 = Math.floor(Math.random() * 6 + 1);
+	const sourceImage2 = "images/dice" + randomNumber2 + ".JPG";
+	const image2 = document.querySelector(".img2").setAttribute("src",sourceImage2 );
+
+	// result
+
+	const a = document.querySelector(".result");
+	if (randomNumber1>randomNumber2) {
+		a.textContent = player1 + " WINS!!";
+	} else if(randomNumber1<randomNumber2){
+		a.textContent = player2 + " WINS!!";
+	}
+	else{
+		a.textContent = "Match DRAW";
+	}
+}
